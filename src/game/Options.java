55bc01cc98df6883fa0534c571178c187f74bd2c
jpaulobf <jpaulobf@gmail.com>
2023-07-01 00:47:50 +0100

    Options (WIP)

--- src/game/Options.java
+++ src/game/Options.java
@@ -3,9 +3,13 @@ package game;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.VolatileImage;
+import util.LoadingStuffs;
 import java.awt.image.BufferedImage;
 import java.awt.GraphicsEnvironment;
 
+/**
+ * 
+ */
 public class Options {
 
     //Scenario variables
@@ -13,7 +17,9 @@ public class Options {
     private int windowWidth             = 0;
     private int windowHeight            = 0;
     private VolatileImage bgBufferImage = null;
+    private BufferedImage optionsLogo   = null;
     private Game gameRef                = null;
+    private final Color GREEN_COLOR     = new Color(51, 152, 101, 255);
 
     /**
      * Constructor
@@ -25,33 +31,33 @@ public class Options {
         this.windowHeight   = windowHeight;
         this.windowWidth    = windowWidth;
         this.gameRef        = game;
+        this.drawBuffer();
     }
 
     /**
      * This private method construct the BG just once.
      * Than, when necessary it is ploted in the backbuffer.
      */
-    private void drawGameOverInBuffer() {
+    private void drawBuffer() {
         if (this.bgd2 == null) {
-            /*
-            this.gameover = (BufferedImage)LoadingStuffs.getInstance().getStuff("gameover");
+            
+            this.optionsLogo = (BufferedImage)LoadingStuffs.getInstance().getStuff("options-logo");
 
             //create a backbuffer image for doublebuffer
             this.bgBufferImage  = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration().createCompatibleVolatileImage(this.windowWidth, this.windowHeight);
             this.bgd2           = (Graphics2D)bgBufferImage.getGraphics();
 
             //paint all bg in black
-            this.bgd2.setBackground(Color.BLACK);
+            this.bgd2.setBackground(GREEN_COLOR);
             this.bgd2.clearRect(0, 0, this.windowWidth, this.windowHeight);
             
-            int imgW = this.gameover.getWidth();
-            int imgH = this.gameover.getHeight();
+            int imgW = this.optionsLogo.getWidth();
+            int imgH = this.optionsLogo.getHeight();
             int imgX = ((this.windowWidth - imgW)/2);
             int imgY = ((this.windowHeight - imgH)/2);
 
-            this.bgd2.drawImage(this.gameover, imgX, imgY, imgW + imgX, imgH + imgY, 
-                                               0, 0, imgW, imgH, null);
-             */
+            this.bgd2.drawImage(this.optionsLogo, imgX, imgY, imgW + imgX, imgH + imgY, 
+                                                  0, 0, imgW, imgH, null);
         }
     }
 
@@ -68,10 +74,20 @@ public class Options {
      */
     public void draw(long frametime) {
         //clear the stage
-        this.gameRef.getG2D().setBackground(Color.BLACK);
+        this.gameRef.getG2D().setBackground(GREEN_COLOR);
         this.gameRef.getG2D().clearRect(0, 0, this.windowWidth, this.windowHeight * 2);
 
         //After construct the bg once, copy it to the graphic device
         this.gameRef.getG2D().drawImage(this.bgBufferImage, 0, 0, null);
+    }
+
+    /**
+     * Control keys in options
+     * @param key
+     */
+    public void move(int key) {
+        if (key == 27) {
+            this.gameRef.changeGameStateToMenu();
+        }
     }
 }

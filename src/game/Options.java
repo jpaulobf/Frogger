826c5f121e4572db82c376e25933c3736330434a
jpaulobf <jpaulobf@gmail.com>
2023-07-04 22:16:25 +0100

    Options (WIP)

--- src/game/Options.java
+++ src/game/Options.java
@@ -14,11 +14,14 @@ public class Options {
 
     //Scenario variables
     private Graphics2D bgd2                 = null;
+    private Game gameRef                    = null;
     private int windowWidth                 = 0;
     private int windowHeight                = 0;
     private VolatileImage bgBufferImage     = null;
+    
+    //Images
+    private BufferedImage selector          = null;
     private BufferedImage optionsLogo       = null;
-
     private BufferedImage labelPlayMusic    = null;
     private BufferedImage labelMusicVolume  = null;
     private BufferedImage labelPlaySFX      = null;
@@ -26,8 +29,19 @@ public class Options {
     private BufferedImage labelHowMany      = null;
     private BufferedImage labelExit         = null;
 
-    private Game gameRef                    = null;
+    //Control variables
+    
+    private final short OG_SELECTOR_X       = 40;
+    private final short OG_SELECTOR_Y       = 285;
+    private final short X_OFFSET            = -20;
+    private short selectorX                 = 40;
+    private short selectorY                 = 285;
+    private byte currentSelectorPos         = 0;
+    
+    //const
     private final Color GREEN_COLOR         = new Color(51, 152, 101, 255);
+    private final short LOGO_X = 800;
+    private final short LOGO_Y = 70;
 
     /**
      * Constructor
@@ -40,6 +54,7 @@ public class Options {
         this.windowWidth        = windowWidth;
         this.gameRef            = game;
 
+        this.selector          = (BufferedImage)LoadingStuffs.getInstance().getStuff("selector");
         this.labelPlayMusic    = (BufferedImage)LoadingStuffs.getInstance().getStuff("label-play-music");
         this.labelMusicVolume  = (BufferedImage)LoadingStuffs.getInstance().getStuff("label-music-volume");
         this.labelPlaySFX      = (BufferedImage)LoadingStuffs.getInstance().getStuff("label-play-sfx");
@@ -67,13 +82,8 @@ public class Options {
             this.bgd2.setBackground(GREEN_COLOR);
             this.bgd2.clearRect(0, 0, this.windowWidth, this.windowHeight);
             
-            int imgW = this.optionsLogo.getWidth();
-            int imgH = this.optionsLogo.getHeight();
-            int imgX = 800;
-            int imgY = 70;
-
-            this.bgd2.drawImage(this.optionsLogo, imgX, imgY, imgW + imgX, imgH + imgY, 
-                                                  0, 0, imgW, imgH, null);
+            //draw
+            this.bgd2.drawImage(this.optionsLogo, LOGO_X, LOGO_Y, null);
         }
     }
 
@@ -82,6 +92,12 @@ public class Options {
      * @param frametime
      */
     public void update(long frametime) {
+        this.selectorX = OG_SELECTOR_X;
+        this.selectorY = OG_SELECTOR_Y;
+        if (this.currentSelectorPos == 5) {
+            this.selectorX = OG_SELECTOR_X + X_OFFSET;
+            this.selectorY = (short)(OG_SELECTOR_Y + 93);
+        }
     }
 
     /**
@@ -96,16 +112,16 @@ public class Options {
         //After construct the bg once, copy it to the graphic device
         this.gameRef.getG2D().drawImage(this.bgBufferImage, 0, 0, null);
 
+        //draw selector
+        this.gameRef.getG2D().drawImage(this.selector, this.selectorX, this.selectorY + (this.currentSelectorPos * 90), null);
 
+        //draw labels
         this.gameRef.getG2D().drawImage(this.labelPlayMusic, 128, 280, null);
         this.gameRef.getG2D().drawImage(this.labelMusicVolume, 128, 370, null);
-        this.gameRef.getG2D().drawImage(this.labelPlaySFX, 128, 459, null);
-        this.gameRef.getG2D().drawImage(this.labelSFXVolume, 128, 548, null);
-        this.gameRef.getG2D().drawImage(this.labelHowMany, 128, 637, null);
+        this.gameRef.getG2D().drawImage(this.labelPlaySFX, 128, 460, null);
+        this.gameRef.getG2D().drawImage(this.labelSFXVolume, 128, 550, null);
+        this.gameRef.getG2D().drawImage(this.labelHowMany, 128, 640, null);
         this.gameRef.getG2D().drawImage(this.labelExit, 38, 817, null);
-
-
-
     }
 
     /**
@@ -114,7 +130,17 @@ public class Options {
      */
     public void move(int key) {
         if (key == 27) {
+            this.currentSelectorPos = 0;
             this.gameRef.changeGameStateToMenu();
+        } else {
+            if (key == 38) {
+                this.currentSelectorPos = (--this.currentSelectorPos<0)?5:this.currentSelectorPos;
+            } else if (key == 40) {
+                this.currentSelectorPos = (byte)(++this.currentSelectorPos%6);
+            } else if (key == 10 || key == 32) {
+                this.currentSelectorPos = 0;
+                this.gameRef.changeGameStateToMenu();
+            }
         }
     }
 }
